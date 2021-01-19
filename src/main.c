1a8f4c6501498df4fe39395adc8db3425fdb6309
pixaliser <77645626+pixaliser@users.noreply.github.com>
2021-01-19 09:42:14 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,11 +2,11 @@
 
   @file         main.c
 
-  @author       Stephen Brennan
+  @author       @pixaliser
 
   @date         Thursday,  8 January 2015
 
-  @brief        LSH (Libstephen SHell)
+  @brief        DISH [Dynamic Shell]
 
 *******************************************************************************/
 
@@ -20,9 +20,9 @@
 /*
   Function Declarations for builtin shell commands:
  */
-int lsh_cd(char **args);
-int lsh_help(char **args);
-int lsh_exit(char **args);
+int dish_cd(char **args);
+int dish_help(char **args);
+int dish_exit(char **args);
 
 /*
   List of builtin commands, followed by their corresponding functions.
@@ -34,12 +34,12 @@ char *builtin_str[] = {
 };
 
 int (*builtin_func[]) (char **) = {
-  &lsh_cd,
-  &lsh_help,
-  &lsh_exit
+  &dish_cd,
+  &dish_help,
+  &dish_exit
 };
 
-int lsh_num_builtins() {
+int dish_num_builtins() {
   return sizeof(builtin_str) / sizeof(char *);
 }
 
@@ -52,13 +52,13 @@ int lsh_num_builtins() {
    @param args List of args.  args[0] is "cd".  args[1] is the directory.
    @return Always returns 1, to continue executing.
  */
-int lsh_cd(char **args)
+int dish_cd(char **args)
 {
   if (args[1] == NULL) {
-    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+    fprintf(stderr, "dish: expected argument to \"cd\"\n");
   } else {
     if (chdir(args[1]) != 0) {
-      perror("lsh");
+      perror("dish");
     }
   }
   return 1;
@@ -69,14 +69,14 @@ int lsh_cd(char **args)
    @param args List of args.  Not examined.
    @return Always returns 1, to continue executing.
  */
-int lsh_help(char **args)
+int dish_help(char **args)
 {
   int i;
-  printf("Stephen Brennan's LSH\n");
+  printf("Stephen Brennan's dish\n");
   printf("Type program names and arguments, and hit enter.\n");
   printf("The following are built in:\n");
 
-  for (i = 0; i < lsh_num_builtins(); i++) {
+  for (i = 0; i < dish_num_builtins(); i++) {
     printf("  %s\n", builtin_str[i]);
   }
 
@@ -89,7 +89,7 @@ int lsh_help(char **args)
    @param args List of args.  Not examined.
    @return Always returns 0, to terminate execution.
  */
-int lsh_exit(char **args)
+int dish_exit(char **args)
 {
   return 0;
 }
@@ -99,7 +99,7 @@ int lsh_exit(char **args)
   @param args Null terminated list of arguments (including program).
   @return Always returns 1, to continue execution.
  */
-int lsh_launch(char **args)
+int dish_launch(char **args)
 {
   pid_t pid;
   int status;
@@ -108,12 +108,12 @@ int lsh_launch(char **args)
   if (pid == 0) {
     // Child process
     if (execvp(args[0], args) == -1) {
-      perror("lsh");
+      perror("dish");
     }
     exit(EXIT_FAILURE);
   } else if (pid < 0) {
     // Error forking
-    perror("lsh");
+    perror("dish");
   } else {
     // Parent process
     do {
@@ -129,7 +129,7 @@ int lsh_launch(char **args)
    @param args Null terminated list of arguments.
    @return 1 if the shell should continue running, 0 if it should terminate
  */
-int lsh_execute(char **args)
+int dish_execute(char **args)
 {
   int i;
 
@@ -138,42 +138,42 @@ int lsh_execute(char **args)
     return 1;
   }
 
-  for (i = 0; i < lsh_num_builtins(); i++) {
+  for (i = 0; i < dish_num_builtins(); i++) {
     if (strcmp(args[0], builtin_str[i]) == 0) {
       return (*builtin_func[i])(args);
     }
   }
 
-  return lsh_launch(args);
+  return dish_launch(args);
 }
 
 /**
    @brief Read a line of input from stdin.
    @return The line from stdin.
  */
-char *lsh_read_line(void)
+char *dish_read_line(void)
 {
-#ifdef LSH_USE_STD_GETLINE
+#ifdef dish_USE_STD_GETLINE
   char *line = NULL;
   ssize_t bufsize = 0; // have getline allocate a buffer for us
   if (getline(&line, &bufsize, stdin) == -1) {
     if (feof(stdin)) {
       exit(EXIT_SUCCESS);  // We recieved an EOF
     } else  {
-      perror("lsh: getline\n");
+      perror("dish: getline\n");
       exit(EXIT_FAILURE);
     }
   }
   return line;
 #else
-#define LSH_RL_BUFSIZE 1024
-  int bufsize = LSH_RL_BUFSIZE;
+#define dish_RL_BUFSIZE 1024
+  int bufsize = dish_RL_BUFSIZE;
   int position = 0;
   char *buffer = malloc(sizeof(char) * bufsize);
   int c;
 
   if (!buffer) {
-    fprintf(stderr, "lsh: allocation error\n");
+    fprintf(stderr, "dish: allocation error\n");
     exit(EXIT_FAILURE);
   }
 
@@ -193,10 +193,10 @@ char *lsh_read_line(void)
 
     // If we have exceeded the buffer, reallocate.
     if (position >= bufsize) {
-      bufsize += LSH_RL_BUFSIZE;
+      bufsize += dish_RL_BUFSIZE;
       buffer = realloc(buffer, bufsize);
       if (!buffer) {
-        fprintf(stderr, "lsh: allocation error\n");
+        fprintf(stderr, "dish: allocation error\n");
         exit(EXIT_FAILURE);
       }
     }
@@ -204,41 +204,41 @@ char *lsh_read_line(void)
 #endif
 }
 
-#define LSH_TOK_BUFSIZE 64
-#define LSH_TOK_DELIM " \t\r\n\a"
+#define dish_TOK_BUFSIZE 64
+#define dish_TOK_DELIM " \t\r\n\a"
 /**
    @brief Split a line into tokens (very naively).
    @param line The line.
    @return Null-terminated array of tokens.
  */
-char **lsh_split_line(char *line)
+char **dish_split_line(char *line)
 {
-  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  int bufsize = dish_TOK_BUFSIZE, position = 0;
   char **tokens = malloc(bufsize * sizeof(char*));
   char *token, **tokens_backup;
 
   if (!tokens) {
-    fprintf(stderr, "lsh: allocation error\n");
+    fprintf(stderr, "dish: allocation error\n");
     exit(EXIT_FAILURE);
   }
 
-  token = strtok(line, LSH_TOK_DELIM);
+  token = strtok(line, dish_TOK_DELIM);
   while (token != NULL) {
     tokens[position] = token;
     position++;
 
     if (position >= bufsize) {
-      bufsize += LSH_TOK_BUFSIZE;
+      bufsize += dish_TOK_BUFSIZE;
       tokens_backup = tokens;
       tokens = realloc(tokens, bufsize * sizeof(char*));
       if (!tokens) {
 		free(tokens_backup);
-        fprintf(stderr, "lsh: allocation error\n");
+        fprintf(stderr, "dish: allocation error\n");
         exit(EXIT_FAILURE);
       }
     }
 
-    token = strtok(NULL, LSH_TOK_DELIM);
+    token = strtok(NULL, dish_TOK_DELIM);
   }
   tokens[position] = NULL;
   return tokens;
@@ -247,7 +247,7 @@ char **lsh_split_line(char *line)
 /**
    @brief Loop getting input and executing it.
  */
-void lsh_loop(void)
+void dish_loop(void)
 {
   char *line;
   char **args;
@@ -255,9 +255,9 @@ void lsh_loop(void)
 
   do {
     printf("> ");
-    line = lsh_read_line();
-    args = lsh_split_line(line);
-    status = lsh_execute(args);
+    line = dish_read_line();
+    args = dish_split_line(line);
+    status = dish_execute(args);
 
     free(line);
     free(args);
@@ -275,7 +275,7 @@ int main(int argc, char **argv)
   // Load config files, if any.
 
   // Run command loop.
-  lsh_loop();
+  dish_loop();
 
   // Perform any shutdown/cleanup.
 
